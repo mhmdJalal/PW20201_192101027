021685dc58442902d5641e36dcce4dd952198735
mhmdjalal_ <muhammaddaud0908@gmail.com>
2020-12-12 11:34:59 +0700

    PHP MVC

---
+++ phpmvc/app/init.php
@@ -0,0 +1,5 @@
+<?php
+define('BASEURL', 'http://localhost/PW20201_192101027/phpmvc/public');
+
+require_once 'core/App.php'; //sebagai class App
+require_once 'core/Controller.php'; //sebagai class Controller
