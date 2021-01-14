44791046525f9082a5b341b7692c5d453e49f9b6
mhmdjalal_ <muhammaddaud0908@gmail.com>
2021-01-14 22:58:12 +0700

    PHP MVC Pertemuan 12

--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -4,20 +4,38 @@
         <?php Flasher::flash(); ?>
         </div>
     </div>
-    <div class="row">
-        <div class="col-6">
-            <!-- Button trigger modal -->
-    
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+
+    <div class="row mb-2">
+        <div class="col-lg-6">
+            <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
             Tambah Data Mahasiswa
             </button>
-            <br><br>
+        </div>
+    </div>
+
+    <div class="row mb-2">
+        <div class="col-lg-6">
+            <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="cari mahasiswa.." name="keyword" id="keyword" autocomplete="off">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                <li class="list-group-item list-group-item d-flex justify-content-between align-items-center">
+                <li class="list-group-item">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">detail</a>
+                    <a onclick="setEdit(<?= $mhs['id']; ?>, '<?= $mhs['nama']; ?>', '<?= $mhs['nim']; ?>', '<?= $mhs['email']; ?>', '<?= $mhs['jurusan']; ?>')" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal">ubah</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onClick="return confirm('Anda yakin akan menghapus data?');">hapus</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">detail</a>
                 </li>
                 <?php endforeach; ?>
             </ul>
@@ -26,30 +44,35 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+        <div class="modal-header">
+            <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
 
-            <div class="modal-body">
-                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" name="nama">
-                    </div>
-                    <div class="form-group">
-                        <label for="nim">NIM</label>
-                        <input type="number" class="form-control" id="nim" name="nim">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan</label>
+            <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+
+        </div>
+
+        <div class="modal-body">
+            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                <input type="hidden" name="id" id="id">
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" id="nama" name="nama" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                    <label for="nim">NIM</label>
+                    <input type="number" class="form-control" id="nim" name="nim" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="email@example.com">
+                </div>
+                <div class="form-group">
+                    <label for="jurusan">Jurusan</label>
                         <select class="form-control" name="jurusan" id="jurusan">
                             <option value="Teknik Informatika">Teknik Informatika</option>
                             <option value="Sistem Informasi">Sistem Informasi</option>
@@ -57,15 +80,33 @@
                             <option value="Teknik Kimia">Teknik Kimia</option>
                             <option value="Teknik Elektro">Teknik Elektro</option>
                         </select>
-                    </div>
-                    </div>
-                    <div class="modal-footer">
+                </div>
+                </div>
+                <div class="modal-footer">
 
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </form>
-            </div>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+        </div>
         </div>
     </div>
 </div>
+
+<script type='text/javascript'>
+
+    function setEdit(id, nama, nim, email, jurusan){
+        var mId = document.getElementById('id');
+        var mNama = document.getElementById('nama');
+        var mNim = document.getElementById('nim');
+        var mEmail = document.getElementById('email');
+        var mJurusans = document.getElementById('jurusan');
+        
+        mId.value = id;
+        mNama.value = nama;
+        mNim.value = nim;
+        mEmail.value = email;
+        mJurusans.value = jurusan
+    }
+
+</script>
